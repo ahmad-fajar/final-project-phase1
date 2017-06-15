952b5e44fa9afa0ca863f59197ac213eb23fea59
kokoh <kokohtan@gmail.com>
2017-06-15 21:38:56 +0700

    welcome

--- routes/orderold.js
+++ routes/orderold.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/",function(req, res, next){
-  res.render("ordernew" , { title: 'ini index' });
+  res.render("orderold" , { title: 'ini index' });
 });
 
 //http://localhost:3000/ordernew/
